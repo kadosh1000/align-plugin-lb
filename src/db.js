e785f2c22b8538d74b1d5acef66500af4816aa4a
Matan Kadosh <kadosh10001@gmail.com>
2021-08-22 12:15:54 +0300

    fix missing function bump version to 1.0.3 update zip

--- src/db.js
+++ src/db.js
@@ -1,81 +1,87 @@
 const { ObjectID, MongoClient } = require("mongodb");
 
 class MongoHelper {
-  constructor() {
-    this.dbUri = "";
-    this.dbName = "";
-  }
-
-  set(settings) {
-    this.dbUri = settings.dbUri;
-    this.dbName = settings.dbName;
-  }
-
-  async getLivingAgents(agentTags) {
-    const collection = "agents";
-    const query = {
-      "status.state":"online"
-	  };
-	
-	const queryOptions = {
-		fields: {
-			name : 1,
-			status : 1
-		}
-	}
-
-    // If specific agentTags specified add to query
-    if (agentTags) {
-      if (!Array.isArray(agentTags)){
-        throw "Tags must be an array"
-      }
-      query.attributes = {
-        $all: agentTags,
-      }
+    constructor() {
+        this.dbUri = "";
+        this.dbName = "";
     }
 
-    const db = await this.getDbClient();
-    const results = await db.collection(collection).find(query, queryOptions).toArray();
-    return results;
-  }
+    set(settings) {
+        this.dbUri = settings.dbUri;
+        this.dbName = settings.dbName;
+    }
 
-  async getAgentsByProjectId(projectId) {
-    const collection = "maps";
-    const query = {
-      "project": projectId,
-	  };
+    async getMapsIdsByProject(projectID) {
+        const collection = "maps";
+        const query = {
+            project: ObjectID(projectID),
+        };
 
-    const db = await this.getDbClient();
-    const results = await db.collection(collection).find(query).toArray();
-    return results;
-  }
+        const queryOptions = {
+            fields: {
+                _id: 1,
+            },
+        };
 
-  async getRunningExecutions(mapsToCheck) {
-    const collection = "mapResults";
-    const query = { 
-		status: "running",
-		map: {
-			$in : mapsToCheck.map(id=> new ObjectID(id))
-		}
-	};
-	const queryOptions = {
-		fields: { 
-			"agentsResults.agent": 1, 
-			"map": 1, 
-			"status": 1 
-		}
-	}
+        const db = await this.getDbClient();
+        const results = await db.collection(collection).find(query, queryOptions).toArray();
+        return results.map((res) => res._id.toString());
+    }
 
-	const db = await this.getDbClient();
-    const results = await db.collection(collection).find(query, queryOptions).toArray();
-    return results;
-  }
+    async getLivingAgents(agentTags) {
+        const collection = "agents";
+        const query = {
+            "status.state": "online",
+        };
 
-  async getDbClient() {
-    const db = await MongoClient.connect(this.dbUri);
+        const queryOptions = {
+            fields: {
+                name: 1,
+                status: 1,
+            },
+        };
 
-    return db.db(this.dbName);
-  }
+        // If specific agentTags specified add to query
+        if (agentTags) {
+            if (!Array.isArray(agentTags)) {
+                throw "Tags must be an array";
+            }
+            query.attributes = {
+                $all: agentTags,
+            };
+        }
+
+        const db = await this.getDbClient();
+        const results = await db.collection(collection).find(query, queryOptions).toArray();
+        return results;
+    }
+
+    async getRunningExecutions(mapsToCheck) {
+        const collection = "mapResults";
+        const query = {
+            status: "running",
+            map: {
+                $in: mapsToCheck.map((id) => new ObjectID(id)),
+            },
+        };
+        const queryOptions = {
+            fields: {
+                "agentsResults.agent": 1,
+                map: 1,
+                status: 1,
+            },
+        };
+
+        const db = await this.getDbClient();
+        const results = await db.collection(collection).find(query, queryOptions).toArray();
+        return results;
+    }
+
+    async getDbClient() {
+        const db = await MongoClient.connect(this.dbUri);
+
+        return db.db(this.dbName);
+    }
 }
 
 module.exports = new MongoHelper();
